e2b4f934623c68d75c0169519e15acb843eb59c6
unknown <fabian179rc@gmail.com>
2022-09-17 13:56:19 -0300

    home created

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,15 +5,46 @@ import { getAllUsers, getUser } from "../redux/actions/user";
 export default function Home() {
   const dispatch = useDispatch();
   const user = useSelector((state) => state.user);
-  console.log(user);
-  const id = "73eaee9e-0c2a-4f4c-900e-6a5f0b002f75";
+  const id = "c96b32be-ccd3-4435-8a8c-e6764823ef6e";
+
   useEffect(() => {
     dispatch(getUser(id));
   }, []);
 
+  const lastOperation = user.operations?.slice(-10);
+
+  function newOperation(e) {
+    e.preventDefault();
+    console.log(e.target.value);
+    //abrir modal
+  }
   return (
     <>
-      <div>Balance {user.balance}</div>
+      {user ? (
+        <div>
+          <h1>Bienvenido {user.username}!</h1>
+          <div>Balance: {user.balance}</div>
+          <button onClick={(e) => newOperation(e)} value="entry">
+            Entry
+          </button>
+          <button onClick={(e) => newOperation(e)} value="egress">
+            Egress
+          </button>
+          <div>
+            <label>Last Ten Operations:</label>
+            {lastOperation?.map((operation, i) => (
+              <ul key={i}>
+                <li>
+                  Concept: {operation.concept} Amount: {operation.amount} Date:{" "}
+                  {operation.date}
+                </li>
+              </ul>
+            ))}
+          </div>
+        </div>
+      ) : (
+        "Loading..."
+      )}
     </>
   );
 }
